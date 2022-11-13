6f9c480b659395d92e450a78bd399fb7fe275300
sheikhshakibhosaain <sheikhshakibpro@gmail.com>
2022-11-13 20:34:25 +0600

    update

--- src/lab5/ExceptionHandling.java
+++ src/lab5/ExceptionHandling.java
@@ -4,12 +4,14 @@ public class ExceptionHandling {
     
     public static void main(String[] args) {
         
-        System.out.println("print 1");
+        int x = 30, y = 0;
+        System.out.println("print 1 before error");
         try {
-            System.out.println("The number is: " + 9/0 );
+            System.out.println("The number is (try): " + x/y );
         } catch(Exception e) {
-            e.printStackTrace();
-            System.out.println(e.getMessage());
+            // e.printStackTrace();
+            // System.out.println(e.getMessage());
+            System.out.println("The number is (catch): " + x/0.0000001 );
         }
         System.out.println("print 2 after exception");
     }
